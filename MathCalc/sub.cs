07717696570f06ad9065713e8d12f6bb3a60cf51
sohailaezz <sohaila>
2022-01-31 12:55:03 +0200

    sohilaEZZ SUB

---
+++ MathCalc/sub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCalc
+{
+    internal class sub
+    {
+        public int x { get; set; }  
+        public int y { get; set; }
+        public int  subS(int x,int y)
+        {
+            return x - y;
+        }
+    }
+}
