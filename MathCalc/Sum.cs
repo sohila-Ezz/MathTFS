af9c87bf95b69503732d3931ca004df5d3e29d48
ShazaEmam <shazaemam1@gmail.com>
2022-01-31 13:33:27 +0200

    Shaza Add Class Sum

---
+++ MathCalc/Sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCalc
+{
+    internal class Sum
+    {
+            public int x{ get; set; }
+            public int y{ get; set; }
+        public int sumNumber (int x, int y)
+        { 
+            return x + y;
+        }
+    }
+}
